5d8f0a3ea99952dc64ceefdf7e8164104e1c53c4
Rafal Sladek <rafal.sladek@gmail.com>
2019-12-31 17:55:24 +0100

    add pm2 custom metrics

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const metrics = require("datadog-metrics");
+const io = require("@pm2/io");
 const config = require("./config");
 const parser = require("./parser");
 const os = require("os");
 const hostname = os.hostname();
+const invocation = io.counter({
+  name: "Invocation"
+});
+const parsing = io.counter({
+  name: "Parse"
+});
 
 const main = () => {
+  invocation.inc();
   Promise.all(
     config.parameters.map(parameter => {
       parser.parseHtml(parameter).then(extendedParameter => {
+        parsing.inc();
         const bufferedMetricsLogger = new metrics.BufferedMetricsLogger({
           host: hostname,
           prefix: "goldcoin.",
--- start.sh
+++ start.sh
@@ -2,4 +2,8 @@
 set -ex
 npm i -g npm yarn pm2 && \
 npm install && \
-DEBUG=metrics pm2-runtime index.js
+DEBUG=metrics pm2 start index.js \
+--name web-scraper \
+--watch \
+--time \
+--log /var/log/web-scraper.log

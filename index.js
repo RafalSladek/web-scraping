ea13fd6229fd9bdcb96d2004bca1e0baa7a92547
Rafal Sladek <rafal.sladek@gmail.com>
2020-02-01 10:57:18 +0100

    remove tags

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ const parseCryptoFGI = () => {
             sendMetric(
               hostname,
               "index.",
-              ["'name:" + title + "'", "'class:" + metricclass + "'"],
+              [],
               metricname,
               metricvalue,
               [],

50b1ea27c85796c15a8e5883ad49eccf9dd7a367
Rafal Sladek <rafal.sladek@gmail.com>
2021-01-12 19:10:37 +0100

    forcing reexecution

--- start.sh
+++ start.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 set -ex
 npm i -g npm yarn pm2 && \
-npm audit fix && npm i
-
+npm i
 pm2 update
 
-DEBUG=metrics pm2 start index.js \
+pm2 delete all
+
+NODE_ENV=prod  DEBUG=metrics pm2 start index.js \
 --name web-scraper \
 --watch \
---time
+--time \
+--no-daemon \
+-f
 
 #--log /var/log/web-scraper.log

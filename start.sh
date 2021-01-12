7c9ca2ccb226940354e672fb3a63c32753b12d68
Rafal Sladek <rafal.sladek@gmail.com>
2021-01-12 19:08:39 +0100

    add pm2 update and audit fix

--- start.sh
+++ start.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 set -ex
 npm i -g npm yarn pm2 && \
-npm install && \
+npm audit fix && npm i
+
+pm2 update
+
 DEBUG=metrics pm2 start index.js \
 --name web-scraper \
 --watch \
 --time
+
 #--log /var/log/web-scraper.log

c961f39468ca0ce1ebf51b68fefbd1ab420ebc2d
Rafal Sladek <rafal.sladek@gmail.com>
2021-01-12 19:39:55 +0100

    remove no daemon

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ex
-npm i -g npm yarn pm2 && \
+npm i -g npm yarn pm2@latest && \
 npm i
 pm2 update
 
@@ -10,7 +10,6 @@ NODE_ENV=prod  DEBUG=metrics pm2 start index.js \
 --name web-scraper \
 --watch \
 --time \
---no-daemon \
 -f
 
 #--log /var/log/web-scraper.log
